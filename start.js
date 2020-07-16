e7f5df5704da87079a6f6558e38b93bfb6049141
Gawtam007 <56129853+Gawtam007@users.noreply.github.com>
2020-07-17 02:07:56 +0530

    Update start.js

--- start.js
+++ start.js
@@ -52,13 +52,14 @@ con.connect (function(err)
   if (err) throw err;
   console.log("Connected!");
     
+  /*
   var sql = "CREATE TABLE urn_values (name VARCHAR(255), address VARCHAR(255))";
   con.query(sql, function (err, result)
   {
     if (err) throw err;
     console.log("Table created");
   });
-    
+   */ 
    var sql = "INSERT INTO urn_values (name, address) VALUES ('Gawtam', 'HELLO')";
    con.query(sql, function (err, result)
    {
