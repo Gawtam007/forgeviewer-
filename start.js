8ec5ce353fdffb6cb94786eb1303512ddf5880e2
Gawtam007 <56129853+Gawtam007@users.noreply.github.com>
2020-07-17 01:53:12 +0530

    Update start.js

--- start.js
+++ start.js
@@ -44,10 +44,16 @@ var con = mysql.createConnection({
   host: "us-cdbr-east-02.cleardb.com",
   user: "b55e94922e7906",
   password: "094305db"
+  database: "heroku_dafec4799bd7c87"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  var sql = "CREATE TABLE urn_values (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
 });
 
